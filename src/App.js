e35ad9251417148731f091186d2cb1ae542b8c05
KellyDRD <93556954+KellyDRD@users.noreply.github.com>
2022-03-01 19:32:50 -0600

    added github link

--- src/App.js
+++ src/App.js
@@ -7,7 +7,16 @@ function App() {
       <div className="App">
         <h1>Weather Search App</h1>
         <WeatherSearch />
-        <small>open source code, by Kelly Roan</small>
+        <small>
+          <a
+            href="https://github.com/KellyDRD/weather_react_homework"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open source code
+          </a>
+          , by Kelly Roan
+        </small>
       </div>
     </div>
   );
