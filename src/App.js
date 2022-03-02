8cd3e16df1781decb4fe9c40d1d3f07087eefdee
KellyDRD <93556954+KellyDRD@users.noreply.github.com>
2022-03-01 19:11:04 -0600

    transfer code from sandbox to vscode

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import WeatherSearch from "./WeatherSearch";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+        <h1>Weather Search App</h1>
+        <WeatherSearch />
+        <small>open source code, by Kelly Roan</small>
+      </div>
     </div>
   );
 }
