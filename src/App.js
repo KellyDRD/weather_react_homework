c7372e48b2d99f924037bf8d1a6eb841715eda83
KellyDRD <93556954+KellyDRD@users.noreply.github.com>
2022-03-03 18:44:28 -0600

    added in bootstrap

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import WeatherSearch from "./WeatherSearch";
 export default function App() {
   return (
     <div className="App">
-      <div className="App">
+      <div className="container">
         <h1>Weather Search App</h1>
         <WeatherSearch />
         <footer>
