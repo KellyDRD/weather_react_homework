3d4d5be00a8d2f8063f52ebc2c39c6151799aece
KellyDRD <93556954+KellyDRD@users.noreply.github.com>
2022-03-13 16:50:14 -0500

    starting over

--- src/App.js
+++ src/App.js
@@ -2,22 +2,5 @@ import "./App.css";
 import WeatherSearch from "./WeatherSearch";
 
 export default function App() {
-  return (
-    <div className="App">
-      <div className="container">
-        <WeatherSearch />
-
-        <footer>
-          <a
-            href="https://github.com/KellyDRD/weather_react_homework"
-            target="_blank"
-            rel="noreferrer"
-          >
-            open source code
-          </a>
-          , by Kelly Roan
-        </footer>
-      </div>
-    </div>
-  );
+  return <div className="App">Weather App</div>;
 }
