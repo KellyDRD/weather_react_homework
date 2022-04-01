44e8f4924ba722ef345ffc8d93c8d1b922e5d73c
KellyDRD <93556954+KellyDRD@users.noreply.github.com>
2022-03-31 19:00:30 -0500

    added in daily forecast api

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,17 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handleResponse(response) {}
+
+  let apiKey = "375047dec9386322672b073ccc06d9ac";
+  let longitude = props.coordinates.lon;
+  let latitude = props.coordinates.lat;
+  let apiUrl = `https://api.openweather.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid${apiKey}&units=imperial`;
+
+  axios.get(apiUrl).then(handleResponse);
   return (
     <div className="WeatherForecast">
       <div className="row">
