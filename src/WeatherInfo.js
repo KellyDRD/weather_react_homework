43ff7f38b70c99d1aef0cc43d9cc209c10e43c6e
KellyDRD <93556954+KellyDRD@users.noreply.github.com>
2022-05-30 13:21:47 -0500

    changed color scheme of weather app

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -15,11 +15,16 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <div className="float-left">
-            <WeatherIcon code={props.data.icon} size={52} />
+          <div className="clearfix">
+            <div className="float-left">
+              <WeatherIcon code={props.data.icon} size={52} />
+            </div>
+            <div className="float-left">
+              <WeatherTemperature celsius={props.data.temperature} />
+            </div>
           </div>
-          <WeatherTemperature celsius={props.data.temperature} />
         </div>
+
         <div className="col-6">
           <ul>
             <li>Humidity: {props.data.humidity}%</li>
